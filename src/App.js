6d0a01ec0de6ce3de60049424e5b82b03f0e0cb9
Bryan Highhill <bryanhighhill@gmail.com>
2023-04-25 20:37:04 -0400

    portfolio site for bryanhighhill.com v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from 'react';
+import "./App.css";
+import AppBanner from './Components/AppBanner/AppBanner';
+import header1 from './Images/bryan-header-img1.png';
+import header2 from './Images/bryan-header-img2.png';
+import footer1 from './Images/bryan-footer-img1.png';
+import footer2 from './Images/bryan-footer-img2.png';
+import nameHeader from './Images/name-header.png';
+
 
 function App() {
+  const [aboutMe, setAboutMe] = useState(false);
+  const [contact, setContact] = useState(false);
+  const [footerHover, setFooterHover] = useState(false);
+  const [greetingPosition, setGreetingPosition] = useState(0);
+  const [headerHover, setHeaderHover] = useState(false);
+  const [headerClicked, setHeaderClicked] = useState(false);
+  const randomNumber = Math.floor(Math.random() * 98);
+
+  useEffect(() => {
+      setTimeout(() => {
+          setGreetingPosition(randomNumber);
+      }, 2500);
+  }, [greetingPosition]);
+
+  const greetings = [
+    {greeting: 'Hello'},
+    {greeting: 'Hallo'},
+    {greeting: 'Përshëndetje'},
+    {greeting: 'Iwi selami newi'},
+    {greeting: 'Marhaba'},
+    {greeting: 'Barev'},
+    {greeting: 'Kamisaki'},
+    {greeting: 'Salam'},
+    {greeting: 'Kaixo'},
+    {greeting: 'Vitaju'},
+    {greeting: 'Hyālō'},
+    {greeting: 'Zdravo'},
+    {greeting: 'Zdraveĭte'},
+    {greeting: 'Haallo'},
+    {greeting: 'Nǐ hǎo'},
+    {greeting: 'Hola'},
+    {greeting: 'Kamusta'},
+    {greeting: 'Moni'},
+    {greeting: 'Bonghjornu'},
+    {greeting: 'Zdravo'},
+    {greeting: 'Ahoj'},
+    {greeting: 'Hej'},
+    {greeting: 'Hallo'}, 
+    {greeting: 'Tere'},
+    {greeting: 'Salām'},
+    {greeting: 'Bula'},
+    {greeting: 'Kumusta'},
+    {greeting: 'Hei'},
+    {greeting: 'Bonjour'},
+    {greeting: 'Dia dhuit'},
+    {greeting: 'Ola'},
+    {greeting: 'Gamarjoba'},
+    {greeting: 'Guten tag'},
+    {greeting: 'Geia'},
+    {greeting: "Mba'éichapa"},
+    {greeting: 'Bonjou'},
+    {greeting: 'Aloha'},
+    {greeting: 'Shalom'},
+    {greeting: 'Namaste'},
+    {greeting: 'Nyob zoo'},
+    {greeting: 'Szia'},
+    {greeting: 'Halló'},
+    {greeting: 'Ndewo'},
+    {greeting: 'Hello'},
+    {greeting: 'Halo'},
+    {greeting: 'Ciao'},
+    {greeting: 'Konichiwa'},
+    {greeting: 'Sälemetsiz be'},
+    {greeting: 'Suostei'},
+    {greeting: 'Mwaramutse'},
+    {greeting: 'Anyeong haseyo'},
+    {greeting: 'Slav'},
+    {greeting: 'Sabaidi'},
+    {greeting: 'Salve'},
+    {greeting: 'Sveika'},
+    {greeting: 'Sveiks'},
+    {greeting: 'Sveiki'},
+    {greeting: 'Moien'},
+    {greeting: 'Salama'},
+    {greeting: 'Selamat pagi'},
+    {greeting: 'Bongu'},
+    {greeting: 'Nǐ hǎo'},
+    {greeting: 'Kia ora'},
+    {greeting: 'Namaskāra'},
+    {greeting: 'Sain uu'},
+    {greeting: 'Niltze Tialli Pialli'},
+    {greeting: 'Ya’at’eeh'},
+    {greeting: 'Namaskāra'},
+    {greeting: 'Hei'},
+    {greeting: 'Salam'},
+    {greeting: 'Cześć'},
+    {greeting: 'Olá'},
+    {greeting: 'Sata srī akāla'},
+    {greeting: 'Akkam'},
+    {greeting: 'Allianchu'},
+    {greeting: 'Bunâ'},
+    {greeting: 'Privet'},
+    {greeting: 'Talofa'},
+    {greeting: 'Thobela'},
+    {greeting: 'Zdravo'},
+    {greeting: 'Dumela'},
+    {greeting: 'Ahoj'},
+    {greeting: 'Zdravo'},
+    {greeting: 'Jambo'},
+    {greeting: 'Hallå'},
+    {greeting: 'Kamusta'},
+    {greeting: 'Ia Orana'},
+    {greeting: 'Li-hó'},
+    {greeting: 'Vanakkam'},
+    {greeting: 'S̄wạs̄dī'},
+    {greeting: 'Tashi delek'},
+    {greeting: 'Mālō e lelei'},
+    {greeting: 'Avuxeni'},
+    {greeting: 'Merhaba'},
+    {greeting: 'Privit'},
+    {greeting: 'Assalāmu Alaykum'},
+    {greeting: 'Salom'},
+    {greeting: 'Xin chào'},
+    {greeting: 'Helo'},
+    {greeting: 'Molo'},
+];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-btns"> 
+        <button className="about-me" onClick={() => setAboutMe(!aboutMe)}>{!aboutMe ? 'About Me' : <b>I'm Done Learning About You</b>}</button>
+        <button className="about-me" onClick={() => setContact(!contact)}>{!contact ? 'Contact' : <b>I've Gotten Your Contact</b>}</button>
+        <button className="about-me" onClick={() => window.open('https://drive.google.com/file/d/1GjeUo86clg8FJu0vKE-aMFigQ--DmNg8/view?usp=share_link')}>Resume</button>
+      </div>
+            {contact && 
+              <div className="bio">
+                <h1 className="hello">Contact</h1>
+                <section className="bio-content">
+                  <a href="mailto:bryanhighhill@gmail.com">Send Email</a>
+                </section>  
+                <section className="bio-content">
+                  <a href="https://www.linkedin.com/in/bryan-highhill/" target="_blank">LinkedIn</a>
+                </section>
+                <section className="bio-content">
+                  <a href="https://github.com/bryanhighhill" target="_blank">Github</a>
+                </section>
+                <section className="bio-content">
+                  <a href="https://www.bryanhighhill.com" target="_blank">Music/Audio</a>
+                </section> 
+              </div>
+            }
+            {aboutMe &&
+              <section className="bio">
+                {/* <h1 className="hello">{greetings[greetingPosition].greeting}!</h1> */}
+                <h1 className="hello">Hello!</h1>
+                <p className="bio-content">
+                  I'm a full-stack software engineer with a background rooted firmly in personal-motivation, creative problem-solving, learning, and creating things. 
+                  It started as a love for music at a young age, writing songs and using technology to make, record, and produce music. This developed into not only a life long passion, 
+                  but a 10 year career working mostly behind-the-scenes in radio and with broadcast technology.
+                  Since 2017, I’ve worked as Digital Content Specialist at a large law firm, producing and managing various digital assets, providing technical support, 
+                  and working with clients.
+                  I’m enthusiastic about software and web development because it’s a field where I get to take advantage of the technical and interpersonal skills I’ve honed. It relies on a growth-mindset and applying creative-thinking in a practical and meaningful way.
+                  Below, I've included some of the web apps I've built. 
+                  <br />
+                  I will be adding more information to explain each of the apps in greater detail, but in the meantime, please hover over an image for a summary of the app, navigate using the arrows, and you can click through to view them on github.  
+                </p>
+              </section>
+            }
+      <section className="header-div">
+        <img 
+          className="header-img"
+          src={nameHeader} 
+          alt="bryan"
+          onMouseEnter={() => setHeaderHover(true)}
+          onMouseLeave={() => setHeaderHover(false)}
+          onClick={() => setHeaderClicked(!headerClicked)}
+        />
+        <img 
+          className="header-img"
+          src={footerHover ? header2 : header1} 
+          alt="bryan"
+          onMouseEnter={() => setFooterHover(true)}
+          onMouseLeave={() => setFooterHover(false)}
+          onClick={() => setHeaderClicked(!headerClicked)}
+        />
+      </section>
+      <AppBanner />
+      <section className="header-div">
+          <img 
+            className="footer-img"
+            src={headerHover ? footer2 : footer1} 
+            alt="bryan"
+            onMouseEnter={() => setHeaderHover(true)}
+            onMouseLeave={() => setHeaderHover(false)}
+        />
+      </section>
     </div>
   );
 }
---
+++ src/Components/AppBanner/AppBanner.jsx
@@ -0,0 +1,144 @@
+import { useState, useEffect } from 'react';
+import shareitageImage from '../../Images/share-itage-screen-shot.png';
+import bearCreekImage from '../../Images/bear-creek-screenshot.png';
+import feedbackLoopImage from '../../Images/feedback-loop-screenshot.png';
+import photoGramImage from '../../Images/photo-gallery-screenshot.png';
+import movieSelectorImage from '../../Images/movie-selector-screenshot.png';
+import calcImage from '../../Images/calcs-screenshot.png';
+import toDoImage from '../../Images/todo-screenshot.png';
+
+import './AppBanner.css';
+
+const AppBanner = () => {
+    const [appIndex, setAppIndex] = useState(1);
+    
+    const appData = [
+        { 
+            id: 1,
+            name: 'Share-Itage',
+            img: shareitageImage,
+            description: 'Share-Itage allows you and your loved ones to share and interact with your family recipes',
+            github: 'https://github.com/bryanhighhill/Share-Itage-App'
+        },
+        { 
+            id: 2,
+            name: 'Bear Creek Lodge',
+            img: bearCreekImage,
+            description: 'Bookkeeping app to help manage bookings at a rental property',
+            github: 'https://bryanhighhill.com/software%20portfolio/PDFs/Bear_Creek_Lodge_documentation.pdf?'
+        },
+        { 
+            id: 3,
+            name: 'Feedback Loop',
+            img: feedbackLoopImage,
+            description: 'App that allows students to rate their progress and submit feedback to instructors after weekly lessons',
+            github: 'https://github.com/bryanhighhill/weekend-redux-feedback-loop'
+        },
+        { 
+            id: 4,
+            name: 'PhotoGram',
+            img: photoGramImage,
+            description: 'Photo app that allows users to add their photos and interact with them',
+            github: 'https://github.com/bryanhighhill/weekend-react-gallery'
+        },
+        { 
+            id: 5,
+            name: 'Movie Selector',
+            img: movieSelectorImage,
+            description: 'App to keep track of your movie collection, the movie genres, and descriptions',
+            github: 'https://github.com/bryanhighhill/weekend-movie-sagas'
+        },
+        { 
+            id: 6,
+            name: 'Server-Side Calculators',
+            img: calcImage,
+            description: 'Server-side calculators',
+            github: 'https://github.com/bryanhighhill/weekend-jquery-server-calculator'
+        },
+        { 
+            id: 7,
+            name: 'To-Do Task App',
+            img: toDoImage,
+            description: 'App to keep track of your to-do list and the task status',
+            github: 'https://github.com/bryanhighhill/weekend-sql-to-do-list'
+        },
+    ];
+
+    const nextIndex = appIndex == appData.length - 1 ? 0 : appIndex + 1;
+    const previousIndex = appIndex === 0 ? appData.length - 1 : appIndex - 1;
+
+    const app = appData[appIndex];
+    const nextApp = appData[nextIndex];
+    const prevApp = appData[previousIndex];
+
+    return (
+        <div className="app-list-border">
+            <br />
+            <p className="apps-header">Apps I've Built</p>
+            <p className="instructions">* navigate using the arrow buttons below, hover over the app for a synopsis, and click through to see it on Github</p>
+            <div className="app-list">
+                <div className="app-div">
+                    <a href={prevApp.github} target="_blank">
+                        <img 
+                            className="image"
+                            src={prevApp.img} 
+                            alt={prevApp.name}
+                        />
+                        <div className="overlay">
+                                <div className="app-title">{prevApp.name}</div>
+                                <div className="description">{prevApp.description}</div>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="app-div">
+                    <a href={app.github} target="_blank">
+                        <img 
+                            className="image"
+                            src={app.img} 
+                            alt={app.name}
+                        />
+                        <div className="overlay">
+                                <div className="app-title">{app.name}</div>
+                                <div className="description">{app.description}</div>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="app-div">
+                    <a href={nextApp.github} target="_blank">
+                        <img 
+                            className="image"
+                            src={nextApp.img} 
+                            alt={nextApp.name}
+                        />
+                        <div className="overlay">
+                                <div className="app-title">{nextApp.name}</div>
+                                <div className="description">{nextApp.description}</div>
+                        </div>
+                    </a>
+                </div>
+                {/* {appData.map((app, index) =>
+                    <div className="app-div">
+                        <a href={app.github} target="_blank">
+                            <img 
+                                // className={imageHover ? "app-icon-hover" : "app-icon"} 
+                                className="image"
+                                src={appData[index].img} 
+                                alt={app.name}
+                            />
+                            <div className="overlay">
+                                    <div className="app-title">{app.name}</div>
+                                    <div className="description">{app.description}</div>
+                            </div>
+                        </a>
+                    </div>
+                )} */}
+            </div>
+            <button onClick={() => setAppIndex(previousIndex)} className="prev-nav-btn">⇦</button>
+            <button onClick={() => setAppIndex(nextIndex)} className="next-nav-btn">⇨</button>
+        </div>
+    );
+  }
+  
+  export default AppBanner;
